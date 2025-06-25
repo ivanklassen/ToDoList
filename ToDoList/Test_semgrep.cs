294e3649bed050516780400a1edfae812a98f582
Ivan Klassen <ivanklassen1@gmail.com>
2025-06-25 22:32:18 +0700

    added Semgrep

---
+++ ToDoList/Test_semgrep.cs
@@ -0,0 +1,31 @@
+﻿namespace ToDoList
+{
+    public class Test_semgrep
+    {
+        public int SomeRiskyOperation()
+        {
+            return 0;
+        }
+        public void errors()
+        {
+            string apiKey = "test_1234567890";
+
+            string userName = "admin";
+
+            var query = "SELECT * FROM Users WHERE Name = '" + userName + "'";
+
+
+
+            try
+            {
+                SomeRiskyOperation();
+            }
+            catch (Exception)  
+            {
+            }
+
+        }
+
+
+    }
+}
