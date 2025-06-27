d5a9bfc82f4c2c08ce82b1c0c17247ade8d4c9f8
Ivan Klassen <ivanklassen1@gmail.com>
2025-06-27 13:08:43 +0700

    check

--- ToDoList/Test_semgrep.cs
+++ ToDoList/Test_semgrep.cs
@@ -2,26 +2,27 @@
 {
     public class Test_semgrep
     {
-        public int SomeRiskyOperation()
-        {
-            return 0;
-        }
-        public void errors()
-        {
-            string apiKey = "test_1574849332674554558";
+    //    public int SomeRiskyOperation()
+    //    {
+    //        return 0;
+    //    }
+    //    public void errors()
+    //    {
+    //        string apiKey = "test_1574849332674554558";
 
-            string userName = "admin";
+    //        string userName = "admin";
 
-            var query = "SELECT * FROM Users WHERE Name = '" + userName + "'";
+    //        var query = "SELECT * FROM Users WHERE Name = '" + userName + "'";
 
 
-            try
-            {
-                SomeRiskyOperation();
-            }
-            catch (Exception) { }
 
-        }
+    //        try
+    //        {
+    //            SomeRiskyOperation();
+    //        }
+    //        catch (Exception) { }
+
+    //    }
 
 
     }
